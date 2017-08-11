9088484976a6c194a379894557e9574c0375fd26
Bradley <bwilsonhunt@gmail.com>
2017-08-11 11:27:09 +0100

    Updating connection info

--- template/connection.php
+++ template/connection.php
@@ -1,4 +1,12 @@
 <?php
+
+
 //$connection = mysqli_connect('HOSTNAME','DB_USERNAME','DB_PASSWORD','DATABASE_NAME');
-$connection = mysqli_connect('localhost', 'root', '', 'ctff'); 
+$connection = mysqli_connect('ctf-mysql:3306', 'root', 'my-secret-pw', 'ctff'); 
+
+// Check connection
+if (mysqli_connect_errno()) {
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
 ?>
